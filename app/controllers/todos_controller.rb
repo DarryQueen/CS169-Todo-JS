55ff7183faef36d7cfc3901767303b1f5c92db71
Darren Zhao <slimfrog@gmail.com>
2016-10-17 23:52:09 +0000

    Create path for todo creation.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,18 +3,28 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
 
+  def create
+    @todo = Todo.create(todo_params)
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+    end
+  end
+
   def toggle
     @todo = Todo.find(params[:id])
     @todo.completed = !@todo.completed
     @todo.save
 
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+    end
   end
 
   private
   
   # Never trust parameters from the scary internet, only allow the white list through.
   def todo_params
-    params.require(:todo).permit(:description)
+    params.require(:todo).permit(:description, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'todos#index'
 
   put 'todo/:id/toggle', :to => 'todos#toggle', :as => 'todo_toggle'
+  put 'todo/create', :to => 'todos#create', :as => 'new_todo'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
